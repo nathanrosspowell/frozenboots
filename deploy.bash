bc652c292e5f96e372f902a18e8825968cf24447
Nathan Ross Powell <nathanrosspowell@gmail.com>
2013-03-03 16:57:37 -0500

    [MOD] Fix build script

--- deploy.bash
+++ deploy.bash
@@ -7,4 +7,14 @@ pushd ..
 if [[ ! -d frozenboots-gh-pages ]]
 then
     git clone git@github.com:nathanrosspowell/frozenboots.git frozenboots-gh-pages
+    pushd frozenboots-gh-pages
+    git checkout gh-pages 
+    popd
 fi
+cp -r frozenboots-flask/website/build/* frozenboots-gh-pages/
+pushd frozenboots-gh-pages 
+git add *
+git commit -m "$@"
+git push 
+popd
+popd
