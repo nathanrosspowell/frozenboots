effa218ef7ba7d18d1268e577136a7a0a3096fe7
Nathan Ross Powell <nathanrosspowell@gmail.com>
2013-03-24 17:33:35 -0400

    [ADD] new deploy and settings

---
+++ settings.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# settings. Authored by Nathan Ross Powell.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+REPRO = "frozenboots"
+USER = "nathanrosspowell"
+BRANCH = "gh-pages"
+    
